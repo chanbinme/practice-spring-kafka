02e4367f2de8ee3c19751f62dd08c3b741c51f7e
chanbinme <gksmfcksqls@gmail.com>
2025-04-03 18:23:16 +0900

    feat: spring kafka 세팅

---
+++ src/main/java/com/chanbinme/practicespringkafka/PracticeSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.chanbinme.practicespringkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chanbinme/practicespringkafka/PracticeSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanbinme.practicespringkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
